1acf703dc30d04840fd9f61f36d1e430fb228dc2
Geada734 <frankgeada@gmail.com>
2022-09-21 19:08:31 -0500

    Added instructionsto the homepage on how to use the app

--- stegonoscreens/src/pages/HomePage.js
+++ stegonoscreens/src/pages/HomePage.js
@@ -2,6 +2,8 @@ import { useContext } from "react";
 
 import AppContext from "../store/app-context";
 
+import classes from "./HomePage.module.css";
+
 import strings from "../static/strings";
 
 function HomePages() {
@@ -9,6 +11,17 @@ function HomePages() {
 
     return <section>
             <h1>{strings.pageTitles.home[appCtx.language]}</h1>
+            <h2>1.- Upload the image with the coded message:</h2>
+            <p className={classes.instructionP}>
+                Choose the all-black image with the coded message in red letters. 
+                This image must not be taller, or wider (or both) than the image you want to hide your 
+                message.
+            </p>
+            <h2>2.- Upload the image you want to hide your message in:</h2>
+            <p className={classes.instructionP}>
+                The image can be larger than the first one on either axis,
+                but it cannot be wider or taller.
+            </p>
         </section>
 };
 
