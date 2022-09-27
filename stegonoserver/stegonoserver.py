794a20da4c157ea5291f76139e4077c9e8efc066
Geada734 <frankgeada@gmail.com>
2022-09-27 13:52:53 -0500

    Managed to get a an image in a REST service call

--- stegonoserver/stegonoserver.py
+++ stegonoserver/stegonoserver.py
@@ -1,4 +1,6 @@
+from PIL import Image
 from flask import Flask
+from flask import request
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
@@ -9,4 +11,9 @@ class DummyAPI(Resource):
         response_body = {"body": "Hello"}
         return response_body
 
-api.add_resource(DummyAPI, "/", endpoint = "/")
+    def post(self):
+        args = request.files["file"]
+        img = Image.open(args)
+        img.show()
+        
+api.add_resource(DummyAPI, "/dummy")
