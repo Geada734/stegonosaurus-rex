81d6cc7ce0724db15b81c940ab45f5995b9e20c7
Geada734 <frankgeada@gmail.com>
2022-09-30 18:55:15 -0500

    Explored option to return processed images as base64 strings

--- stegonoserver/stegonoserver.py
+++ stegonoserver/stegonoserver.py
@@ -1,6 +1,9 @@
+import base64
 from PIL import Image
+from io import BytesIO
 from flask import Flask
 from flask import request
+from flask import send_file
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
@@ -14,6 +17,11 @@ class DummyAPI(Resource):
     def post(self):
         args = request.files["file"]
         img = Image.open(args)
-        img.show()
-        
+
+        buffered = BytesIO()
+        img.save(buffered, format="PNG")
+        img_str = base64.b64encode(buffered.getvalue())
+
+        return {"result": img_str.decode("utf-8")}
+
 api.add_resource(DummyAPI, "/dummy")
