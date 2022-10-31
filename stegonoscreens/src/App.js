8f9dbabe657981c516c2e3832f0b60940bc5b64c
Geada734 <frankgeada@gmail.com>
2022-10-30 18:46:13 -0600

    Added a not found page

--- stegonoscreens/src/App.js
+++ stegonoscreens/src/App.js
@@ -10,6 +10,7 @@ import Layout from './layout/Layout';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import FAQPage from './pages/FAQPage';
+import NotFoundPage from './pages/NotFoundPage';
 import ErrorModal from './components/ErrorModal';
 import LoadingModal from './components/LoadingModal';
 import ImageDisplayModal from './components/ImageDisplayModal';
@@ -29,9 +30,10 @@ function App() {
   return (<main>
       <Layout className={classes.app}>
         <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/faq" element={<FAQPage />} />
+            <Route path='/' element={<HomePage />} />
+            <Route path='/about' element={<AboutPage />} />
+            <Route path='/faq' element={<FAQPage />} />
+            <Route path='*' element={<NotFoundPage />} />
         </Routes>
       </Layout>
       <LoadingModal showModal={showLoading} title={strings.loadingModal.loadingFAQs[appCtx.language]}/>
---
+++ stegonoscreens/src/pages/NotFoundPage.js
@@ -0,0 +1,8 @@
+function NotFoundPage() {
+    return <section>
+        <h1>Not Found</h1>
+        <span>The page you were looking for does not exist</span>
+    </section>
+};
+
+export default NotFoundPage;
