2f7c302a801bf799f8b4a758b6da6ba0ae848ba6
Geada734 <frankgeada@gmail.com>
2023-06-19 18:32:45 -0600

    Improved error handling

--- stegonoserver/app/src/utils/stegono_utils.py
+++ stegonoserver/app/src/utils/stegono_utils.py
@@ -5,45 +5,66 @@ from io import BytesIO
 
 from PIL import Image
 from flask import Response
-from stegonosaurus import stegofunctions as sf
+from stegonosaurus import stegofunctions as sf, stegoexceptions as se
+
+from . import error_handlers as err_handlers
 
 
 def decode(img: Image, filename: str, mode: str, response: Response) -> Response:
-    """Decode wrapper function, returns a response with the result."""    
-    img = sf.decode(img, mode)
+    """Decode wrapper function, returns a response with the result."""
+    try:
+        img = sf.decode(img, mode)
 
-    buffered = BytesIO()
-    img.save(buffered, format="PNG")
-    img_str = base64.b64encode(buffered.getvalue())
+        buffered = BytesIO()
+        img.save(buffered, format="PNG")
+        img_str = base64.b64encode(buffered.getvalue())
 
-    response.status_code = 200
-    response.data = json.dumps({
-            "result": img_str.decode("utf-8"),
-            "filename": "decoded_" + filename
-    })
+        response.status_code = 200
+        response.data = json.dumps({
+                "result": img_str.decode("utf-8"),
+                "filename": "decoded_" + filename
+        })
 
-    img.close()
+        img.close()
+
+    # Handle exceptions from stegonosaurus.
+    except (se.StegonosaurusIncorrectFormatError, se.StegonosaurusInvalidDecodeModeError) as err:
+        img.close()
+        if isinstance(err, se.StegonosaurusIncorrectFormatError):
+            return err_handlers.handle_exception(err, "wrongFormat", err.message)
+
+        if isinstance(err, se.StegonosaurusInvalidDecodeModeError):
+            return err_handlers.handle_exception(err, "wrongDecodeMode", err.message)
 
     return response
 
 
-def encode(coded_file: Image, img_file: str, filename: str, response: Response) -> Response:
-    """Encode wrapper function, returns a response with the result."""    
-    coded = Image.open(coded_file)
-    img = Image.open(img_file)
-    img = sf.encode(coded, img)
+def encode(coded: Image, img: Image, filename: str, response: Response) -> Response:
+    """Encode wrapper function, returns a response with the result."""
+    try:
+        img = sf.encode(coded, img)
 
-    buffered = BytesIO()
-    img.save(buffered, format="PNG")
-    img_str = base64.b64encode(buffered.getvalue())
+        buffered = BytesIO()
+        img.save(buffered, format="PNG")
+        img_str = base64.b64encode(buffered.getvalue())
 
-    response.status_code = 200
-    response.data = json.dumps({
-            "result": img_str.decode("utf-8"),
-            "filename": "encoded_" + filename
-    })
+        response.status_code = 200
+        response.data = json.dumps({
+                "result": img_str.decode("utf-8"),
+                "filename": "encoded_" + filename
+        })
 
-    coded.close()
-    img.close()
+        coded.close()
+        img.close()
+
+    # Handle exceptions from stegonosaurus.
+    except(se.StegonosaurusIncorrectFormatError, se.StegonosaurusIncorrectSizeError) as err:
+        coded.close()
+        img.close()
+        if isinstance(err, se.StegonosaurusIncorrectFormatError):
+            return err_handlers.handle_exception(err, "wrongFormat", err.message)
+
+        if isinstance(err, se.StegonosaurusIncorrectSizeError):
+            return err_handlers.handle_exception(err, "wrongSize", err.message)
 
     return response
