acf0483a6826638fc4fa6e255f3a93f1e8b55e60
Geada734 <frankgeada@gmail.com>
2023-06-27 15:51:45 -0600

    All done with stegono controller tests

--- stegonoserver/app/tests/test_stegono_api.py
+++ stegonoserver/app/tests/test_stegono_api.py
@@ -20,7 +20,7 @@ def test_decode(testegonoserver, timestamp_now):
     api.add_resource(stegono_con.DecodeAPI, "/decode")
 
     # Setting request body.
-    img = open("app/tests/static/encoded_black-kitten.png", "rb")
+    img = open("app/tests/static/file.png", "rb")
 
     headers = {"Authorization": "Bearer " + token}
     body_data = {
@@ -121,7 +121,7 @@ def test_bad_jwt_decode(testegonoserver, timestamp_now, test_config):
     data = json.loads(response.data)
 
     assert (response.status_code == 401 and data == {"error_codename": "invalidToken",
-                                                     "error_message": ("Invalid JWT")})
+                                                     "error_message": "Invalid JWT"})
 
 
 def test_expired_jwt_decode(testegonoserver, timestamp_fixed):
@@ -150,7 +150,7 @@ def test_expired_jwt_decode(testegonoserver, timestamp_fixed):
     data = json.loads(response.data)
 
     assert (response.status_code == 401 and data == {"error_codename": "invalidToken",
-                                                     "error_message": ("Invalid JWT")})
+                                                     "error_message": "Invalid JWT"})
 
 
 def test_future_jwt_decode(testegonoserver, timestamp_future):
@@ -179,7 +179,7 @@ def test_future_jwt_decode(testegonoserver, timestamp_future):
     data = json.loads(response.data)
 
     assert (response.status_code == 401 and data == {"error_codename": "invalidToken",
-                                                     "error_message": ("Invalid JWT")})
+                                                     "error_message": "Invalid JWT"})
 
 
 def test_not_a_jwt_decode(testegonoserver):
@@ -208,7 +208,7 @@ def test_not_a_jwt_decode(testegonoserver):
     data = json.loads(response.data)
 
     assert (response.status_code == 401 and data == {"error_codename": "invalidToken",
-                                                     "error_message": ("Invalid JWT")})
+                                                     "error_message": "Invalid JWT"})
 
 
 def test_empty_auth_header_decode(testegonoserver):
@@ -235,7 +235,7 @@ def test_empty_auth_header_decode(testegonoserver):
     data = json.loads(response.data)
 
     assert (response.status_code == 401 and data == {"error_codename": "invalidToken",
-                                                     "error_message": ("Invalid JWT")})
+                                                     "error_message": "Invalid JWT"})
 
 
 def test_no_auth_header_decode(testegonoserver):
@@ -262,11 +262,11 @@ def test_no_auth_header_decode(testegonoserver):
     data = json.loads(response.data)
 
     assert (response.status_code == 401 and data == {"error_codename": "invalidToken",
-                                                     "error_message": ("Invalid JWT")})
+                                                     "error_message": "Invalid JWT"})
 
 
 def test_valid_captcha_decode(testegonoserver, timestamp_now, mocker):
-    """Test valid decoding."""
+    """Test valid captcha."""
     mocker.patch("src.controllers.stegono_controller.sec.validate_captcha", return_value = True)
     token = sec.encode_token(config, timestamp_now)
 
@@ -276,7 +276,7 @@ def test_valid_captcha_decode(testegonoserver, timestamp_now, mocker):
     api.add_resource(stegono_con.DecodeAPI, "/decode")
 
     # Setting request body.
-    img = open("app/tests/static/encoded_black-kitten.png", "rb")
+    img = open("app/tests/static/file.png", "rb")
 
     headers = {"Authorization": "Bearer " + token}
     body_data = {
@@ -298,7 +298,7 @@ def test_valid_captcha_decode(testegonoserver, timestamp_now, mocker):
 
 
 def test_invalid_captcha_decode(testegonoserver, timestamp_now, mocker):
-    """Test valid decoding."""
+    """Test invalid captcha."""
     mocker.patch("src.controllers.stegono_controller.sec.validate_captcha", return_value = False)
     token = sec.encode_token(config, timestamp_now)
 
@@ -308,18 +308,232 @@ def test_invalid_captcha_decode(testegonoserver, timestamp_now, mocker):
     api.add_resource(stegono_con.DecodeAPI, "/decode")
 
     # Setting request body.
-    img = open("app/tests/static/encoded_black-kitten.png", "rb")
+    img = open("app/tests/static/file.png", "rb")
 
     headers = {"Authorization": "Bearer " + token}
     body_data = {
             "filename": "file.png",
             "mode": "t",
             "img": (img, "file.png"),
-            "captchaValue": "valid"
+            "captchaValue": "invalid"
         }
 
     client = app.test_client()
     response = client.post("/decode", data=body_data, headers=headers,
+                           content_type="multipart/form-data")
+    img.close()
+    data = json.loads(response.data)
+
+    assert (response.status_code == 500 and data=={'error_codename': 'unknown',
+                                                   'error_message': 'Unknown internal error'})
+
+
+# Image encoding tests:
+def test_encode(testegonoserver, timestamp_now):
+    """Test valid encoding."""
+    token = sec.encode_token(config, timestamp_now)
+
+    # Setting endpoint for testing.
+    app = testegonoserver
+    api = Api(app)
+    api.add_resource(stegono_con.EncodeAPI, "/encode")
+
+    # Setting request body.
+    img = open("app/tests/static/file.png", "rb")
+    coded = open("app/tests/static/coded.png", "rb")
+
+    headers = {"Authorization": "Bearer " + token}
+    body_data = {
+            "filename": "file.png",
+            "img": (img, "file.png"),
+            "coded": (coded, "coded.png")
+        }
+
+    client = app.test_client()
+    response = client.post("/encode", data=body_data, headers=headers,
+                           content_type="multipart/form-data")
+    img.close()
+    data = json.loads(response.data)
+
+    assert (response.status_code == 200
+            and data["filename"] == "encoded_file.png"
+            and "result" in data)
+
+
+def test_missing_image_encode(testegonoserver, timestamp_now):
+    """Test missing images."""
+    token = sec.encode_token(config, timestamp_now)
+
+    # Setting endpoint for testing.
+    app = testegonoserver
+    api = Api(app)
+    api.add_resource(stegono_con.EncodeAPI, "/encode")
+
+    # Setting request body.
+    headers = {"Authorization": "Bearer " + token}
+    body_data = {
+            "filename": "file.png"
+        }
+
+    client = app.test_client()
+    response = client.post("/encode", data=body_data, headers=headers,
+                           content_type="multipart/form-data")
+
+    data = json.loads(response.data)
+
+    assert (response.status_code == 500 and data == {"error_codename": "malformedRequest",
+                                                     "error_message": "Malformed request."})
+
+
+def test_not_image_file_encode(testegonoserver, timestamp_now):
+    """Test encoding to a non image file."""
+    token = sec.encode_token(config, timestamp_now)
+
+    # Setting endpoint for testing.
+    app = testegonoserver
+    api = Api(app)
+    api.add_resource(stegono_con.EncodeAPI, "/encode")
+
+    # Setting request body.
+    img = open("app/tests/static/image.txt", "rb")
+    coded = open("app/tests/static/coded.png", "rb")
+
+    headers = {"Authorization": "Bearer " + token}
+    body_data = {
+            "filename": "file.png",
+            "img": (img, "file.png"),
+            "coded": (coded, "coded.png")
+        }
+
+    client = app.test_client()
+    response = client.post("/encode", data=body_data, headers=headers,
+                           content_type="multipart/form-data")
+    img.close()
+    data = json.loads(response.data)
+
+    assert (response.status_code == 500 and data == {"error_codename": "wrongFormat",
+                                                     "error_message": ("The file provided is not " +
+                                                                       "a valid image.")})
+
+
+def test_not_image_coded_encode(testegonoserver, timestamp_now):
+    """Test encoding from a non image coded file."""
+    token = sec.encode_token(config, timestamp_now)
+
+    # Setting endpoint for testing.
+    app = testegonoserver
+    api = Api(app)
+    api.add_resource(stegono_con.EncodeAPI, "/encode")
+
+    # Setting request body.
+    img = open("app/tests/static/file.png", "rb")
+    coded = open("app/tests/static/image.txt", "rb")
+
+    headers = {"Authorization": "Bearer " + token}
+    body_data = {
+            "filename": "file.png",
+            "img": (img, "file.png"),
+            "coded": (coded, "coded.png")
+        }
+
+    client = app.test_client()
+    response = client.post("/encode", data=body_data, headers=headers,
+                           content_type="multipart/form-data")
+    img.close()
+    data = json.loads(response.data)
+
+    assert (response.status_code == 500 and data == {"error_codename": "wrongFormat",
+                                                     "error_message": ("The file provided is not " +
+                                                                       "a valid image.")})
+
+
+def test_bad_jwt_encode(testegonoserver, timestamp_now, test_config):
+    """Test invalid JWT."""
+    token = sec.encode_token(test_config, timestamp_now)
+
+    # Setting endpoint for testing.
+    app = testegonoserver
+    api = Api(app)
+    api.add_resource(stegono_con.EncodeAPI, "/encode")
+
+    # Setting request body.
+    img = open("app/tests/static/file.png", "rb")
+    coded = open("app/tests/static/coded.png", "rb")
+
+    headers = {"Authorization": "Bearer " + token}
+    body_data = {
+            "filename": "file.png",
+            "img": (img, "file.png"),
+            "coded": (coded, "coded.png")
+        }
+
+    client = app.test_client()
+    response = client.post("/encode", data=body_data, headers=headers,
+                           content_type="multipart/form-data")
+    img.close()
+    data = json.loads(response.data)
+
+    assert (response.status_code == 401 and data == {"error_codename": "invalidToken",
+                                                     "error_message": "Invalid JWT"})
+
+
+def test_valid_captcha_encode(testegonoserver, timestamp_now, mocker):
+    """Test valid captcha."""
+    mocker.patch("src.controllers.stegono_controller.sec.validate_captcha", return_value = True)
+    token = sec.encode_token(config, timestamp_now)
+
+    # Setting endpoint for testing.
+    app = testegonoserver
+    api = Api(app)
+    api.add_resource(stegono_con.EncodeAPI, "/encode")
+
+    # Setting request body.
+    img = open("app/tests/static/file.png", "rb")
+    coded = open("app/tests/static/coded.png", "rb")
+
+    headers = {"Authorization": "Bearer " + token}
+    body_data = {
+            "filename": "file.png",
+            "img": (img, "file.png"),
+            "coded": (coded, "coded.png"),
+            "captchaValue": "valid"
+        }
+
+    client = app.test_client()
+    response = client.post("/encode", data=body_data, headers=headers,
+                           content_type="multipart/form-data")
+    img.close()
+    data = json.loads(response.data)
+
+    assert (response.status_code == 200
+            and data["filename"] == "encoded_file.png"
+            and "result" in data)
+
+
+def test_invalid_captcha_encode(testegonoserver, timestamp_now, mocker):
+    """Test invalid captcha."""
+    mocker.patch("src.controllers.stegono_controller.sec.validate_captcha", return_value = False)
+    token = sec.encode_token(config, timestamp_now)
+
+    # Setting endpoint for testing.
+    app = testegonoserver
+    api = Api(app)
+    api.add_resource(stegono_con.EncodeAPI, "/encode")
+
+    # Setting request body.
+    img = open("app/tests/static/file.png", "rb")
+    coded = open("app/tests/static/coded.png", "rb")
+
+    headers = {"Authorization": "Bearer " + token}
+    body_data = {
+            "filename": "file.png",
+            "img": (img, "file.png"),
+            "coded": (coded, "coded.png"),
+            "captchaValue": "invalid"
+        }
+
+    client = app.test_client()
+    response = client.post("/encode", data=body_data, headers=headers,
                            content_type="multipart/form-data")
     img.close()
     data = json.loads(response.data)
