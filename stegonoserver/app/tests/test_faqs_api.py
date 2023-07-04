ebaaa05e217d54e8894a716670fb6cfb289a3cb4
Geada734 <frankgeada@gmail.com>
2023-07-04 14:25:16 -0600

    Just mongo exception tests pending

--- stegonoserver/app/tests/test_faqs_api.py
+++ stegonoserver/app/tests/test_faqs_api.py
@@ -54,8 +54,8 @@ def test_get_faqs(testegonoserver, timestamp_now, mocker, mockgo_db):
                     }]})
 
 
-def test_wrapper_on_faqs_endpoint(testegonoserver, timestamp_now, test_config):
-    """Test calling FAQs endpoint with a bad JWT."""
+def test_bad_jwt_get_faqs(testegonoserver, timestamp_now, test_config):
+    """Test calling get FAQs endpoint with a bad JWT."""
     token = sec.encode_token(test_config, timestamp_now)
 
     # Setting endpoint for testing.
@@ -68,6 +68,115 @@ def test_wrapper_on_faqs_endpoint(testegonoserver, timestamp_now, test_config):
     client = app.test_client()
 
     response = client.get("/faqs", headers=headers)
+    data = json.loads(response.data)
+
+    assert (response.status_code == 401 and
+            data == {
+                "error_codename": "invalidToken",
+                "error_message": "Invalid JWT"
+            })
+
+
+def test_expired_jwt_get_faqs(testegonoserver, timestamp_fixed):
+    """Test calling get FAQs endpoint with an expired JWT."""
+    token = sec.encode_token(config, timestamp_fixed)
+
+    # Setting endpoint for testing.
+    app = testegonoserver
+    api = Api(app)
+    api.add_resource(faqs_con.FAQsAPI, "/faqs")
+
+    headers = {"Authorization": "Bearer " + token}
+
+    client = app.test_client()
+
+    response = client.get("/faqs", headers=headers)
+    data = json.loads(response.data)
+
+    assert (response.status_code == 401 and
+            data == {
+                "error_codename": "invalidToken",
+                "error_message": "Invalid JWT"
+            })
+
+
+def test_future_jwt_get_faqs(testegonoserver, timestamp_future):
+    """Test calling get FAQs endpoint with an future JWT."""
+    token = sec.encode_token(config, timestamp_future)
+
+    # Setting endpoint for testing.
+    app = testegonoserver
+    api = Api(app)
+    api.add_resource(faqs_con.FAQsAPI, "/faqs")
+
+    headers = {"Authorization": "Bearer " + token}
+
+    client = app.test_client()
+
+    response = client.get("/faqs", headers=headers)
+    data = json.loads(response.data)
+
+    assert (response.status_code == 401 and
+            data == {
+                "error_codename": "invalidToken",
+                "error_message": "Invalid JWT"
+            })
+
+
+def test_not_a_jwt_get_faqs(testegonoserver):
+    """Test calling get FAQs endpoint with something that isn't a JWT."""
+    token = "Obviously invalid token."
+
+    # Setting endpoint for testing.
+    app = testegonoserver
+    api = Api(app)
+    api.add_resource(faqs_con.FAQsAPI, "/faqs")
+
+    headers = {"Authorization": "Bearer " + token}
+
+    client = app.test_client()
+
+    response = client.get("/faqs", headers=headers)
+    data = json.loads(response.data)
+
+    assert (response.status_code == 401 and
+            data == {
+                "error_codename": "invalidToken",
+                "error_message": "Invalid JWT"
+            })
+
+
+def test_empty_auth_header_get_faqs(testegonoserver):
+    """Test calling get FAQs endpoint with an empty auth header."""
+    # Setting endpoint for testing.
+    app = testegonoserver
+    api = Api(app)
+    api.add_resource(faqs_con.FAQsAPI, "/faqs")
+
+    headers = {"Authorization": "Bearer "}
+
+    client = app.test_client()
+
+    response = client.get("/faqs", headers=headers)
+    data = json.loads(response.data)
+
+    assert (response.status_code == 401 and
+            data == {
+                "error_codename": "invalidToken",
+                "error_message": "Invalid JWT"
+            })
+
+
+def test_no_auth_header_get_faqs(testegonoserver):
+    """Test calling get FAQs endpoint with no auth header."""
+    # Setting endpoint for testing.
+    app = testegonoserver
+    api = Api(app)
+    api.add_resource(faqs_con.FAQsAPI, "/faqs")
+
+    client = app.test_client()
+
+    response = client.get("/faqs")
     data = json.loads(response.data)
 
     assert (response.status_code == 401 and
@@ -100,10 +209,166 @@ def test_rate_faq(testegonoserver, timestamp_now, mocker, mockgo_db):
     response = client.put("/faqs", headers=headers, data=body_data)
     data = json.loads(response.data)
     faq = mockgo_db.stegonodb.faqs.find_one({"id": 2}, {"_id": 0})
-    print(faq)
 
     assert (response.status_code == 200 and
             data == {
                 "message": "Vote submitted succesfully."
             } and
             faq["rating"] == 1)
+
+
+def test_bad_jwt_rate_faq(testegonoserver, test_config, timestamp_now):
+    """Test FAQs rating with a bad JWT."""
+    token = sec.encode_token(test_config, timestamp_now)
+
+    # Setting endpoint for testing.
+    app = testegonoserver
+    api = Api(app)
+    api.add_resource(faqs_con.FAQsAPI, "/faqs")
+
+    headers = {"Authorization": "Bearer " + token}
+    body_data = {
+                    "id": 2, 
+                    "vote": 1
+                }
+
+    client = app.test_client()
+
+    response = client.put("/faqs", headers=headers, data=body_data)
+    data = json.loads(response.data)
+
+    assert (response.status_code == 401 and
+            data == {
+                "error_codename": "invalidToken",
+                "error_message": "Invalid JWT"
+            })
+
+
+def test_expired_jwt_rate_faq(testegonoserver,  timestamp_fixed):
+    """Test FAQs rating with an expired JWT."""
+    token = sec.encode_token(config, timestamp_fixed)
+
+    # Setting endpoint for testing.
+    app = testegonoserver
+    api = Api(app)
+    api.add_resource(faqs_con.FAQsAPI, "/faqs")
+
+    headers = {"Authorization": "Bearer " + token}
+    body_data = {
+                    "id": 2, 
+                    "vote": 1
+                }
+
+    client = app.test_client()
+
+    response = client.put("/faqs", headers=headers, data=body_data)
+    data = json.loads(response.data)
+
+    assert (response.status_code == 401 and
+            data == {
+                "error_codename": "invalidToken",
+                "error_message": "Invalid JWT"
+            })
+
+
+def test_future_jwt_rate_faq(testegonoserver,  timestamp_future):
+    """Test FAQs rating with a future JWT."""
+    token = sec.encode_token(config, timestamp_future)
+
+    # Setting endpoint for testing.
+    app = testegonoserver
+    api = Api(app)
+    api.add_resource(faqs_con.FAQsAPI, "/faqs")
+
+    headers = {"Authorization": "Bearer " + token}
+    body_data = {
+                    "id": 2, 
+                    "vote": 1
+                }
+
+    client = app.test_client()
+
+    response = client.put("/faqs", headers=headers, data=body_data)
+    data = json.loads(response.data)
+
+    assert (response.status_code == 401 and
+            data == {
+                "error_codename": "invalidToken",
+                "error_message": "Invalid JWT"
+            })
+
+
+def test_not_a_jwt_rate_faq(testegonoserver):
+    """Test FAQs rating with something that isn't a JWT."""
+    token = "Obviously invalid JWT."
+
+    # Setting endpoint for testing.
+    app = testegonoserver
+    api = Api(app)
+    api.add_resource(faqs_con.FAQsAPI, "/faqs")
+
+    headers = {"Authorization": "Bearer " + token}
+    body_data = {
+                    "id": 2, 
+                    "vote": 1
+                }
+
+    client = app.test_client()
+
+    response = client.put("/faqs", headers=headers, data=body_data)
+    data = json.loads(response.data)
+
+    assert (response.status_code == 401 and
+            data == {
+                "error_codename": "invalidToken",
+                "error_message": "Invalid JWT"
+            })
+
+
+def test_empty_auth_header_rate_faq(testegonoserver):
+    """Test FAQs rating with an empty auth header."""
+    # Setting endpoint for testing.
+    app = testegonoserver
+    api = Api(app)
+    api.add_resource(faqs_con.FAQsAPI, "/faqs")
+
+    headers = {"Authorization": "Bearer "}
+    body_data = {
+                    "id": 2, 
+                    "vote": 1
+                }
+
+    client = app.test_client()
+
+    response = client.put("/faqs", headers=headers, data=body_data)
+    data = json.loads(response.data)
+
+    assert (response.status_code == 401 and
+            data == {
+                "error_codename": "invalidToken",
+                "error_message": "Invalid JWT"
+            })
+
+
+def test_no_auth_header_rate_faq(testegonoserver):
+    """Test FAQs rating with no auth header."""
+    # Setting endpoint for testing.
+    app = testegonoserver
+    api = Api(app)
+    api.add_resource(faqs_con.FAQsAPI, "/faqs")
+
+    body_data = {
+                    "id": 2, 
+                    "vote": 1
+                }
+
+    client = app.test_client()
+
+    response = client.put("/faqs", data=body_data)
+    data = json.loads(response.data)
+
+    assert (response.status_code == 401 and
+            data == {
+                "error_codename": "invalidToken",
+                "error_message": "Invalid JWT"
+            })
